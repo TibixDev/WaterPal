22b541a1d79ebd091c633791f05e46fcdcdc873f
Sayykii <martin40lmg@gmail.com>
2022-06-26 13:16:21 +0300

    [🚰] Background color change to gradient

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,6 @@ import Home from "./views/Home.vue";
 </script>
 
 <template>
-    <h1>Hello, world!</h1>
     <Home></Home>
 </template>
 
@@ -12,8 +11,11 @@ import Home from "./views/Home.vue";
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+body {
+background: #74ebd5; /* fallback for old browsers */
+background: -webkit-linear-gradient(to right, #74ebd5, #acb6e5); /* Chrome 10-25, Safari 5.1-6 */
+background: linear-gradient(to right, #74ebd5, #acb6e5);
 }
 </style>
